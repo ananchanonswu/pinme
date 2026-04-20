b00b378cf5a1cf7c451a837008ce9119d460e882
AnanchanonSWU <ananchanon.pansuwan@g.swu.ac.th>
2026-04-21 01:06:14 +0700

    update

--- pinme_website/playwright.config.js
+++ pinme_website/playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: './front_end/tests',
   timeout: 30000,
   expect: {
     timeout: 5000
